090d4c95253aa8d3a822aed5aa28b05dde6128c3
Jesica Velazquez <jesica.velazquez@mercadolibre.com>
2021-10-06 11:10:48 -0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import NavbarComponent from './components/Navbar';
 import ListContainer from './components/ListContainer/ListContainer';
+import Footer from './components/Footer/Footer';
 import './App.scss';
 
 function App() {
@@ -13,18 +14,21 @@ function App() {
   };
 
   return (
-    <main className={theme ? 'light' : 'dark'}>
-      <NavbarComponent
-        theme={theme}
-        handleTheme={handleTheme}
-        favArray={favArray}
-      />
-      <ListContainer
-        theme={theme}
-        favArray={favArray}
-        setFavArray={setFavArray}
-      />
-    </main>
+    <>
+      <main className={theme ? 'light' : 'dark'}>
+        <NavbarComponent
+          theme={theme}
+          handleTheme={handleTheme}
+          favArray={favArray}
+        />
+        <ListContainer
+          theme={theme}
+          favArray={favArray}
+          setFavArray={setFavArray}
+        />
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import './styles/footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className='description-footer'>
+        Made with ❤️ for after class-6/10 by -
+        <a href='https://www.linkedin.com/in/%E2%9A%A1-jesica-velazquez-840a44120/'>
+          Jesica Velazquez
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
