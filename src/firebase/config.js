f4923c421e2592f00435fd18cf445479a6f25044
Jesica Velazquez <jesica.velazquez@mercadolibre.com>
2021-10-29 10:08:14 -0300

    local variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyD2wb2aFk3o6l-BcAtBEnCopWArdCwnB0k',
-  authDomain: 'rickandmorty-coderhouse.firebaseapp.com',
-  projectId: 'rickandmorty-coderhouse',
-  storageBucket: 'rickandmorty-coderhouse.appspot.com',
-  messagingSenderId: '542169037300',
-  appId: '1:542169037300:web:f317aa2ca10ed87849e1a1',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
